f047bb5d1ba72cb42d869114c093f3bab638cf0c
Vlad vilams <vladkinall@gmail.com>
2022-03-28 11:20:45 +1000

    awdasfasd

--- matrix_product.cpp
+++ matrix_product.cpp
@@ -31,42 +31,7 @@ int main()
 		}
 	}
 	if (k1 == k2)
-	{
-		// выделение памяти
-		int **c = new int *[n];
-		for (int i = 0; i < n; i++) 
-		{
-		    c[i] = new int[m];
-		}
-		// заполнение матрицы C
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				c[i][j] = 0;
-				for (int k = 0; k < n; k++)
-				{
-					c[i][j] += a[i][k] * b[k][j];
-				}
-			}
-		}
-		// вывод матрицы C
-		cout << "Матрица C = A * B: " << endl;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				cout << c[i][j] << " ";
-			}
-			cout << endl;
-		}
-		// освобождение памяти
-        for (int i = 0; i < n; i++) 
-        {
-            delete[] c[i];
-        }
-        delete[] c;
-    }
+	
 	{
 		cout << "Перемножение невозможно" << endl;
 	}
